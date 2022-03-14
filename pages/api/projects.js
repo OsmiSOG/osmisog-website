aff18bdc63afb512f3eb276579b4ffba28f08399
Santiago Otalora <santiago.ooo6@gmail.com>
2022-03-14 11:47:03 -0500

    create basic api for personal information

---
+++ pages/api/projects.js
@@ -0,0 +1,155 @@
+export default function handler(req, res) {
+    res.status(200).json([
+        {
+            name: 'Accedo',
+            img: '/logo-accedo.svg',
+            description: `Accedo es la empresa en la que trabajo actualmente, 
+                en mi busqueda de un lugar mejor de trabajo ademas de un sitio donde realizar mis practicas me tope con accedo,
+                aqui he trabajado en proyectos para el ciclo de vida del personal, en el web page de reclutamiento dentro de la empresa,
+                proyectos externos como alltruismo y noisesharks.
+                Dentro de mis principales tareas y retos que he superado aqui han sido, la adquisición y diseño de requerimientos para diferentes modulos del sistema interno,
+                dentro de ellos el sistema de reclutamiento, soporte de proyectos como implementacion de mejoras, cambios, y actualizacion de los diferentes depedencias de los proyectos.`,
+            links: [
+                {
+                    type: 'page',
+                    url: 'https://www.accedotechnologies.com/'
+                }
+            ],
+            technology_stack: {
+                description: 'Las Principales tecnologias que uso',
+                technologies: [
+                    'Laravel', 'Vue', 'Mysql', 'Sail Docker', 'Javascript'
+                ]
+            }
+        },
+        {
+            name: 'PokePage',
+            img: '/pokeapi.png',
+            description: `Este es uno de mis proyectos en los que he practicado y entrenado tecnologias como React y Vue,
+                por lo que tengo dos versiones de este mismo proyecto, uno con cada tecnologia respectivamente,
+                en donde consumo el 
+                <Link type="link" target="_blank" href="https://pokeapi.co/"> api pokemon </Link> 
+                listandolos, y ver el detalle de cada pokemon.`,
+            links: [
+                {
+                    type: 'page',
+                    url: 'https://osmisog.github.io/pokemon-react/',
+                    title: 'PokePage React'
+                },
+                {
+                    type: 'github',
+                    url: 'https://github.com/OsmiSOG/pokemon-react',
+                    title: 'PokePage GitHub React'
+                },
+                {
+                    type: 'page',
+                    url: 'https://pokemon-api-d2eb5.web.app/',
+                    title: 'PokePage Vue'
+                },
+                {
+                    type: 'github',
+                    url: 'https://github.com/OsmiSOG/pokemon-project',
+                    title: 'PokePage GitHub Vue'
+                }
+            ],
+            technology_stack: {
+                description: 'Tecnologias implementadas',
+                technologies: [
+                    'React', 'Vue', 'Vuex', 'firebase', 'api pokemon'
+                ]
+            }
+        },
+        {
+            name: 'Process Pluss',
+            img: '/logo-process-plus.png',
+            description: `Este fue en la primer empresa y de igual modo proyecto en el que trabaje,
+            se trata de un ERP para la misma empresa de publicidad en el cual se buscaba tener control de las diferentes campañas que se tenian, 
+            desde creación de presupuestos, hasta la asignación de tareas entre el ejecutivo y su equipo de creativos.
+            Este proyecto se realizo con laravel, aunque debido a la falta de cultura tecnologica, y bajo presupuesto para el desarrollo, 
+            el codigo era dificil de mantener, poco escalable, no seguia ningún principio de SOLID o DRY, por lo que apesar de usar laravel apenas si se implementaba un 1% del freamwork.
+            Desafortunadamente no cuentan con una pagina web en el que se puedan dar a conocer.`,
+            links: [
+                {
+                    type: 'page',
+                    url: 'http://process.grupotesta.com.co:8989/Pruebas/public/Ingreso'
+                }
+            ],
+            technology_stack: {
+                description: 'Las Principales tecnologias que uso',
+                technologies: [
+                    'Laravel', 'Vue', 'Mysql', 'Sail Docker', 'Javascript'
+                ]
+            }
+        },
+        {
+            name: 'Valorator Uniminuto',
+            img: 'https://rebrand.ly/valoratorUniminuto',
+            description: `Este fue el proyecto que trabaje dentro del semillero universitario,
+                el cual su area de investigación son las smart cities y blockchain.
+                El proyecto esta principalmente orientado hacia las smart cities, 
+                en donde se buscaba empezar a implementar esta filosofia en ciudades como Bogotá, Colombia, 
+                ayudando a dar un avance hacia este aspecto, con una orientación hacia el e-gobenance, 
+                para lograr obtener una valoración de la ciudad apartir de la percepción de cada persona,
+                desde tres aspectos diferentes; salud, segurida y ambiente, 
+                de tal modo que con los reportes generados por el aplicativo los gobiernos puedan tomar acción de acuerdo a la calificación del indice de bienestar obtenido.`,
+            links: [
+                {
+                    type: 'page',
+                    url: 'https://rebrand.ly/valoratorUniminuto'
+                }
+            ],
+            technology_stack: {
+                description: 'Tecnologias implementadas',
+                technologies: [
+                    'PHP', 'Javascript', 'mysql'
+                ]
+            }
+        },
+        {
+            name: 'puzzle 15',
+            img: '/puzzle.png',
+            description: `Este fue uno de mis proyectos universitarios, 
+                se trata del famoso y sencillo juego de puzzle 15, 
+                el cual consiste en un tablero de 4 x 4 piezas cuadradas, las cuales estan numeradas, 
+                en donde la pieza 16 no esta, dejando un espació entre las fichas,
+                con el fin de ordenarlas del 1 al 15.`,
+            links: [
+                {
+                    type: 'page',
+                    url: 'https://osmisog.github.io/Puzzle15/'
+                }
+            ],
+            technology_stack: {
+                description: 'Tecnologias implementadas',
+                technologies: [
+                    'Javascript'
+                ]
+            }
+        },
+        {
+            name: 'Otros Proyectos',
+            img: '/others.png',
+            description: `Dentro de mi trayectoria, he realizado una gran variedad de proyectos, 
+                tanto universitario, como a nivel personal y autodidacta,
+                algunos de ellos son; algunos de ellos son un aplicativo para colaborar en el apredizaje de idiomas con Node,
+                trabajos de simulación e inteligencia artificial con python, mineria de datos con R, 
+                algunos proyectos implementando implementando las bases de la criptografia, entre otros...`,
+            links: [
+                {
+                    type: 'github',
+                    url: 'https://github.com/OsmiSOG'
+                },
+                {
+                    type: 'github',
+                    url: 'https://github.com/osmiOTALORAGUERRERO'
+                }
+            ],
+            technology_stack: {
+                description: 'Tecnologias usadas',
+                technologies: [
+                    'PHP', 'Javascript', 'Python', 'R', 'Java', 'Ruby', 'React', 'Vue'
+                ]
+            }
+        },
+    ])
+}
