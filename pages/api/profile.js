443cf36f65584c1b32b28691cb3f289de2715c2d
Santiago Otalora <santiago.ooo6@gmail.com>
2022-03-15 14:12:56 -0500

    Creating api data

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -1,47 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { profile } from "../../data/profile";
 
 export default function handler(req, res) {
-  res.status(200).json({
-    nickname: 'OsmiSOG',
-    full_name: 'Osmi Santiago Otálora Guerrero',
-    short_name: 'Santiago Otalora',
-    name: 'Osmi Santiago',
-    last_name: 'Otalora Guerrero',
-    introduction: `Soy Desarrollador de software Full Stack, apasionado por la tecnología.
-      He desarrollado proyectos en diferentes lenguajes de programación,
-      sin embargo he tenido mayor experiencia en PHP junto con su Freamwork Laravel,
-      y desde el front he trabajado con Vue y React.`,
-    about: `Hola!!!, soy Santiago Otalora, aunque muchos me dicen Osmi, no es que sea un apodo o sobre nombre, y aunque parezca mentira es mi primer nombre,
-      ya que por cosas del destino mi padre se le vino ese nombre y como que le sono chevere pues asi quedo, no podia hacer nada en ese momento para impedirlo. <br/><br/>
-      Despues de esta pequeña anecdota sobre mi nombre, me presento.
-      Soy de Colombia, he vivido toda mi vida aqui hasta el momento, tengo 22 años, y me apasiona la tecnología, siempre estoy aprendiendo cosas nuevas, y pngo lo mejor de mi.
-      desde que estaba en el colegio siempre queria hacer cosas con tecnología, ya que me fasinaba todo lo que hacia,
-      por lo que cuando llego el momento de decidir que queria estudiar me decidí por ingeniería de sistema, empeze a los 17 años, y me encanto,
-      desde entonces programar se volvio parte de mi vida.
-      Recuerdo que mi primer programa que hice fue hechó en Java, y fue con el que empeze hacer varias cositas, luego conocí a PHP y me gusto bastante, desde entonces ha sido mi lenguage principal junto con Laravel,
-      aunque tambien he hecho cositas con node y python, y con javascript ultimamente he estado usando React y Vue. <br/><br/>
-      Dentro de mi experiencia en proyectos laborales lo obtuve mientras aun continuaba con mis estudios, y fue como desarrollador web, mi primer experiencia dentro de lo que me gusta hacer,
-      y fue muy sorprente porque aunque tomo tiempo en conseguirlo ya que de la mayoria de los lugares a los que me postulaba no me llamaban seguramente por la falta de experiencia, ellos se arriesgaron conmigo,
-      y demostre mi capacidad en las pruebas y me contrataron, hasta que llego de realizar mis practicas profesionales,
-      sin embargo en donde no setia que pudiera avanzar y que de seguro podria encontrar otro lugar en los que pudiera enfrentar nuevos retos y aprender cosas nuevas,
-      por lo que tome la decisión de buscar en otro sitio para realizar mis practicas, durante mi busqueda viaje y salí a conocer un poco más de Colombia, hasta que boom!!!
-      una nueva oportunidad se me presento y la tome, y definitavemente fue una buena decision, he aprendido más cosas y he resuelto nuevos, y para mi sorpresa terminando las practicas me ofrecioeron la oportidad de continuar ahí, y acepte,
-      por lo que actualmente me encuentro trabajando con Vue y Laravel.`,
-    skills: [
-      'PHP',
-      'Java',
-      'JavaScript',
-      'Laravel',
-      'Vue',
-      'React',
-      'MySql',
-    ],
-    activities: [
-      'Aprender',
-      'Desarrollar',
-      'Salir en bici',
-      'Jugar VideoJuegos',
-      'Ver Series y Peliculas',
-    ]
-  })
+  res.status(200).json(profile)
 }
